ec8d349d8a046a5f1e649f4d97be7b5d1a5fa191
parisneha1412 <snehaprayag1412@gmail.com>
2025-06-30 10:11:34 -0400

    Update and rename LaunchPage.java to LaunchPageOld.java

--- src/test/java/pages/LaunchPageOld.java
+++ src/test/java/pages/LaunchPageOld.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-public class LaunchPage {
+public class LaunchPageOld {
 	By passwordLocator = By.name("password");
 	WebDriver driver;
 	
